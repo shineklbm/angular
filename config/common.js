e3a89d58d657e24f46200d6faed83315f2b2edad
Shine Sudarsanan <shineklbm@gmail.com>
2015-05-11 17:52:47 +0530

    Create common.js

---
+++ config/common.js
@@ -0,0 +1,14 @@
+[
+	{
+	"file_name" : "Common Scripts",
+	"file_url" : "assets/js/common.js"
+	},
+	{
+	"file_name" : "JSTree",
+	"file_url" : "assets/js/jstree.min.js"
+	},
+	{
+	"file_name" : "Script 3",
+	"file_url" : "assets/images/member3.png"
+	}
+]
